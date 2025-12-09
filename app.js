67492981bc0e69b4632c69dea2e53e78a5541ebc
Warren Wiens <wwiens@gmail.com>
2025-12-09 16:00:27 -0600

    Added a function

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ function sum(a, b) {
   return a + b;
 }
 
+function muliply(a, b) {
+  return a * b;
+}
+
 console.log("App running. Sum(2,3) =", sum(2, 3));
 
-module.exports = sum;
+module.exports = sum, muliply;
