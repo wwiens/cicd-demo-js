d1ffdd438dcecd4ae858b8ff844133532c69b675
Warren Wiens <wwiens@gmail.com>
2025-12-09 16:05:28 -0600

    Revisions

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ function sum(a, b) {
   return a + b;
 }
 
-function muliply(a, b) {
-  return a * b;
-}
-
 console.log("App running. Sum(2,3) =", sum(2, 3));
 
-module.exports = sum, muliply;
+module.exports = sum;
